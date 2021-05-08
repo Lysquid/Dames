0549e2de06f04b6f27dbec335e0ddf95d4270a5a
Lysquid <>
2021-05-08 12:37:06 +0000

    indentation

--- Affichage.java
+++ Affichage.java
@@ -1,114 +1,114 @@
 import java.util.Scanner;
 
-public class Affichage {
+final public class Affichage {
 
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
+  public static final String ANSI_RESET = "\u001B[0m";
+  public static final String ANSI_BLACK = "\u001B[30m";
+  public static final String ANSI_RED = "\u001B[31m";
+  public static final String ANSI_GREEN = "\u001B[32m";
+  public static final String ANSI_YELLOW = "\u001B[33m";
+  public static final String ANSI_BLUE = "\u001B[34m";
+  public static final String ANSI_PURPLE = "\u001B[35m";
+  public static final String ANSI_CYAN = "\u001B[36m";
+  public static final String ANSI_WHITE = "\u001B[37m";
 
-    private static final String SYMBOLE_COIN = "■";
-    private static final String SYMBOLE_CASE = "▐█▌";
+  private static final String SYMBOLE_COIN = "■";
+  private static final String SYMBOLE_CASE = "▐█▌";
 
-    private static Scanner scanner = new Scanner(System.in);
+  private static Scanner scanner = new Scanner(System.in);
 
-    public static void afficher(Plateau plateau) {
+  public static void afficher(Plateau plateau) {
 
-        effaceEcran();
-        String affichage = "";
-        String bordure_ligne = "";
+    effaceEcran();
+    String affichage = "";
+    String bordure_ligne = "";
 
-        bordure_ligne += ANSI_BLACK + SYMBOLE_COIN;
-        for (int i = 0; i < plateau.taille; i++) {
-            bordure_ligne += " " + (i + 1) % 10;
-        }
-        bordure_ligne += " " + SYMBOLE_COIN + ANSI_RESET;
-
-        affichage += bordure_ligne + "\n";
-        for (int y = 0; y < plateau.taille; y++) {
-            affichage += ANSI_BLACK + (y + 1) % 10 + ANSI_RESET;
-            if (y % 2 == 1) {
-                affichage += " ";
-            }
-            for (int x = 0; x < plateau.taille; x++) {
-                if ((y + x) % 2 == 0) {
-                    affichage += ANSI_BLACK + SYMBOLE_CASE + ANSI_RESET;
-                } else {
-                    Piece piece = plateau.getPiece(x, y);
-                    if (piece instanceof Piece) {
-                        affichage += piece.toString();
-                    } else {
-                        affichage += " ";
-                    }
-                }
-            }
-            if (y % 2 == 0) {
-                affichage += " ";
-            }
-            affichage += ANSI_BLACK + (y + 1) % 10 + ANSI_RESET + "\n";
-        }
-        affichage += bordure_ligne + "\n";
-        System.out.println(affichage);
+    bordure_ligne += ANSI_BLACK + SYMBOLE_COIN;
+    for (int i = 0; i < plateau.taille; i++) {
+      bordure_ligne += " " + (i + 1) % 10;
     }
+    bordure_ligne += " " + SYMBOLE_COIN + ANSI_RESET;
 
-    public static void effaceEcran() {
-        /**
-         * Fonction fournie sur discord pour effacer l'écran
-         */
-        try {
-            String os = System.getProperty("os.name");
-            if (os.contains("Windows")) {
-                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-            } else if (os.contains("Linux")) {
-                new ProcessBuilder("bash", "-c", "clear").inheritIO().start().waitFor();
-            } else {
-                new ProcessBuilder("cmd", "/c", "clear").inheritIO().start().waitFor();
-            }
-        } catch (final Exception e) {
-            System.out.println("Erreur : " + e);
+    affichage += bordure_ligne + "\n";
+    for (int y = 0; y < plateau.taille; y++) {
+      affichage += ANSI_BLACK + (y + 1) % 10 + ANSI_RESET;
+      if (y % 2 == 1) {
+        affichage += " ";
+      }
+      for (int x = 0; x < plateau.taille; x++) {
+        if ((y + x) % 2 == 0) {
+          affichage += ANSI_BLACK + SYMBOLE_CASE + ANSI_RESET;
+        } else {
+          Piece piece = plateau.getPiece(x, y);
+          if (piece instanceof Piece) {
+            affichage += piece.toString();
+          } else {
+            affichage += " ";
+          }
         }
+      }
+      if (y % 2 == 0) {
+        affichage += " ";
+      }
+      affichage += ANSI_BLACK + (y + 1) % 10 + ANSI_RESET + "\n";
     }
+    affichage += bordure_ligne + "\n";
+    System.out.println(affichage);
+  }
 
-    public static int[][] demanderCoup(Joueur joueur) {
-        boolean formatLegal = false;
-        int[][] coup = new int[2][2];
-        String[] coord = new String[2];
-        while (!formatLegal) {
-            System.out.print(joueur.toString() + " > ");
+  public static void effaceEcran() {
+    /**
+      * Fonction fournie sur discord pour effacer l'écran
+      */
+    try {
+      String os = System.getProperty("os.name");
+      if (os.contains("Windows")) {
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+      } else if (os.contains("Linux")) {
+        new ProcessBuilder("bash", "-c", "clear").inheritIO().start().waitFor();
+      } else {
+        new ProcessBuilder("cmd", "/c", "clear").inheritIO().start().waitFor();
+      }
+    } catch (final Exception e) {
+      System.out.println("Erreur : " + e);
+    }
+  }
 
-            try {
-                coord[0] = String.valueOf(scanner.nextInt());
-                coord[1] = String.valueOf(scanner.nextInt());
-                for (int i = 0; i < 2; i++) {
-                    if (coord[i].length() < 2) {
-                        coord[i] = "0" + coord[i];
-                    }
-                    for (int j = 0; j < 2; j++) {
-                        coup[i][j] = Math.floorMod(Character.getNumericValue(coord[i].charAt(j)) - 1, 10);
-                    }
-                }
-                // System.out.println(coup[0][0] + "" + coup[0][1] + " " + coup[1][0] + "" +
-                // coup[1][1]);
-                formatLegal = true;
-            } catch (Exception e) {
-                // System.out.println(e);
-                System.out.println("Format invalide , réesseyez. Exemple : 72 63");
-                while (scanner.hasNext()) {
-                    scanner.next();
-                }
+  public static int[][] demanderCoup(Joueur joueur) {
+    boolean formatLegal = false;
+    int[][] coup = new int[2][2];
+    String[] coord = new String[2];
+    while (!formatLegal) {
+      System.out.print(joueur.toString() + " > ");
+
+      try {
+        coord[0] = String.valueOf(scanner.nextInt());
+        coord[1] = String.valueOf(scanner.nextInt());
+        for (int i = 0; i < 2; i++) {
+            if (coord[i].length() < 2) {
+              coord[i] = "0" + coord[i];
             }
-
+            for (int j = 0; j < 2; j++) {
+              coup[i][j] = Math.floorMod(Character.getNumericValue(coord[i].charAt(j)) - 1, 10);
+            }
         }
-        return coup;
-    }
+        // System.out.println(coup[0][0] + "" + coup[0][1] + " " + coup[1][0] + "" +
+        // coup[1][1]);
+        formatLegal = true;
+      } catch (Exception e) {
+        // System.out.println(e);
+        System.out.println("Format invalide , réesseyez. Exemple : 72 63");
+        while (scanner.hasNext()) {
+          scanner.next();
+        }
+      }
 
-    public static void quitter() {
-        scanner.close();
     }
+    return coup;
+  }
+
+  public static void quitter() {
+    scanner.close();
+  }
 
 }
--- Plateau.java
+++ Plateau.java
@@ -1,35 +1,35 @@
 public class Plateau {
 
-    public int taille;
-    private Piece[][] grille;
+  public int taille;
+  private Piece[][] grille;
 
-    public Plateau(int taille) {
-        grille = new Piece[taille][taille];
-        this.taille = taille;
-    }
+  public Plateau(int taille) {
+    grille = new Piece[taille][taille];
+    this.taille = taille;
+  }
 
-    public void configurationInitiale(Joueur J1, Joueur J2) {
+  public void configurationInitiale(Joueur J1, Joueur J2) {
 
-        for (int y = 0; y < grille.length; y++) {
-            for (int x = (y + 1) % 2; x < grille[y].length; x += 2) {
-                if (y < taille / 2 - 1) {
-                    setPiece(new Pion(J2), x, y);
-                } else if (y > (taille + 1) / 2) {
-                    setPiece(new Pion(J1), x, y);
-                }
-            }
+    for (int y = 0; y < grille.length; y++) {
+      for (int x = (y + 1) % 2; x < grille[y].length; x += 2) {
+        if (y < taille / 2 - 1) {
+          setPiece(new Pion(J2), x, y);
+        } else if (y > (taille + 1) / 2) {
+          setPiece(new Pion(J1), x, y);
         }
-
+      }
     }
 
-    public Piece getPiece(int x, int y) {
-        return grille[x][y];
-    }
+  }
 
-    public void setPiece(Piece piece, int x, int y) {
-        grille[x][y] = piece;
-        piece.x = x;
-        piece.y = y;
-    }
+  public Piece getPiece(int x, int y) {
+    return grille[x][y];
+  }
+
+  public void setPiece(Piece piece, int x, int y) {
+    grille[x][y] = piece;
+    piece.x = x;
+    piece.y = y;
+  }
 
 }
