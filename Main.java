62a61f36043e60dfc4d3f525b9bd838955be6dfc
Hazlyde <romainbenoit21560.com>
2021-05-13 12:40:27 +0200

    rajouté class Main

---
+++ Main.java
@@ -0,0 +1,13 @@
+/**
+ * L'IDE en ligne que nous avons utilsé (repl.it) requiert l'existence d'une
+ * classe nommée Main.java pour lancé le programme. Elle ne fait donc
+ * qu'appeller la fonction main de la classe principale
+ */
+
+public class Main {
+
+  public static void main(String[] args) {
+    MaClassePrincipale.main(args);
+  }
+
+}
